5eb254e40bdaa9a36ef641c69053237efb3aad47
takuma <takuma.y0524@gmail.com>
2020-01-29 15:14:29 +0900

    Fix: light_sensor

--- firmware/src/light_sensor.hpp
+++ firmware/src/light_sensor.hpp
@@ -7,42 +7,64 @@
 #include "queue.hpp"
 #include "utils.hpp"
 
-template<class PhotoTransister>
-class light_sensor // consumes timer 2
+namespace gb7
 {
-    PhotoTransister pin;
-    uint16_t recieved_data = 0;
-
-    static inline constexpr uint8_t prologue = 0b1011011;
-    static inline constexpr uint8_t epilogue = 0b1011001;
-
-public:
-    light_sensor()
+    template<class PhotoTransister>
+    class light_sensor
     {
-        gb7::timer::timer2::init();
+        PhotoTransister pin;
+        uint16_t recieved_data = 0;
+        uint32_t timer_id = 0;
 
-        using namespace gb7::timer::literals;
-        gb7::timer::timer2::invoke_every(500_us, +[](void* ls) {
-            decltype(pin) pin;
-            static uint32_t buffer = 0;
+        uint32_t data = 0;
 
-            buffer = (buffer << 1) & (pin ? 1 : 0);
+        static inline constexpr uint8_t prologue = 0b1011001;
+        static inline constexpr uint8_t epilogue = 0b1001001;
 
-            if (static_cast<uint8_t>(buffer) == light_sensor::epologue && (buffer >> 24) == light_sensor::prologue)
-            {
-                static_cast<light_sensor<PhotoTransister>*>(ls)->recieved_data = (buffer >> 8) && 0xffff;
-            }
-        }, this);
-    }
-    ~light_sensor()
-    {
-        gb7::timer::timer2::invoke_in(0, nullptr);
-    }
+    public:
+        light_sensor()
+        {
+            gb7::timer::multitimer::init();
 
-    uint16_t get_recieved_data()
-    {
-        return recieved_data;
-    }
-};
+            using namespace gb7::timer::literals;
+            timer_id = gb7::timer::multitimer::invoke_every(1_ms, 0, +[](void* ls) {
+                decltype(pin) pin;
+
+                static_cast<decltype(this)>(ls)->data <<= 1;
+                static_cast<decltype(this)>(ls)->data |= pin ? 0 : 1;
+
+                if (static_cast<uint8_t>(static_cast<decltype(this)>(ls)->data >> 24) == static_cast<decltype(this)>(ls)->prologue &&
+                    static_cast<uint8_t>(static_cast<decltype(this)>(ls)->data >> 0 ) == static_cast<decltype(this)>(ls)->epilogue)
+                {
+                PORTC |= 0b10000;
+                    uint16_t shifted = static_cast<uint16_t>(static_cast<decltype(this)>(ls)->data >> 8);
+                    static_cast<decltype(this)>(ls)->recieved_data =
+                        (shifted & (1 << 0 )) >> 0 |
+                        (shifted & (1 << 2 )) >> 1 |
+                        (shifted & (1 << 4 )) >> 2 |
+                        (shifted & (1 << 6 )) >> 3 |
+                        (shifted & (1 << 8 )) >> 4 |
+                        (shifted & (1 << 10)) >> 5 |
+                        (shifted & (1 << 12)) >> 6 |
+                        (shifted & (1 << 14)) >> 7;
+                }
+            }, this);
+        }
+        ~light_sensor()
+        {
+            gb7::timer::multitimer::cancel_invocation(timer_id);
+        }
+
+        uint8_t get_recieved_data()
+        {
+            return recieved_data;
+        }
+
+        uint32_t get_raw_data()
+        {
+            return data;
+        }
+    };
+} // namespace gb7
 
 #endif // LIGHT_SENSOR_HPP
